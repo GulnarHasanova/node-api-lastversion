db1979aba1ae4919d131bacaa003fe684beac1b3
gulnar <gulnar@gmail.com>
2023-01-18 06:55:46 -0800

    commit

---
+++ client/src/Pages/Add/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Index() {
+  
+    
+
+
+  return (
+   <form>
+  
+    <input placeholder='name'></input>
+    <br/>
+    <input placeholder='price'type={'number'}></input>
+    <br/>
+    <input placeholder='count'type={'number'}></input>
+    
+   </form>
+  )
+}
+
+export default Index
---
+++ client/src/Pages/Table/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useState,useEffect } from 'react';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Index() {
+
+
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        axios.get("http://localhost:8080/products")
+        .then(res => setData(res.data))
+
+    },[])
+
+    console.log(data);
+
+    const handleDelete = (id)=>{
+        axios.delete(`http://localhost:8080/products/${id}`)
+       const deleteItem = data.filter(x=>x._id!==id)
+       setData(deleteItem)
+
+    }
+
+
+
+  return (
+    <div className='container'>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+         
+            <TableCell>Name</TableCell>
+            <TableCell >Id</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell >Count</TableCell>
+            <TableCell >Edit</TableCell>
+            <TableCell >Delete</TableCell>
+            <TableCell >View</TableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { data.map((row) => (
+            <TableRow
+              key={row._id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+             
+              <TableCell >{row.name}</TableCell>
+              <TableCell >{row._id}</TableCell>
+              <TableCell>{row.price}</TableCell>
+              <TableCell >{row.count}</TableCell>
+              <TableCell ><button className='btn btn-primary'>Edit</button></TableCell>
+              <TableCell ><button className='btn btn-danger' onClick={()=>{handleDelete(row._id) }}>Delete</button></TableCell>
+              <TableCell ><button className='btn btn-primary'>Go to details</button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+  )
+}
+
+export default Index
